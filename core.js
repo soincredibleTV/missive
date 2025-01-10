caa56aa65004ee037187ecf16c5a75f0c860dc80
soincredibleTV <developer@soincredible.tv>
2025-01-10 23:26:37 +0200

    Update core.js

--- core.js
+++ core.js
@@ -1,3 +1,16 @@
+export async function fetchFromGoogleCloud(conversationId) {
+    const endpoint = `/api/v1/applications/64bea2c89335ca76865eedef/records/list/${conversationId}`;
+    const method = 'POST';
+    const data = { limit: 2 };
+    try {
+        const result = await callGoogleFunction(endpoint, method, data);
+        console.log("API Call Result:", result); // Logging for debugging
+        return result;
+    } catch (error) {
+        console.error("Error fetching from Google Cloud:", error);
+    }
+}
+
 export async function callGoogleFunction(endpoint, method, data) {
     try {
         const response = await fetch('https://us-central1-missive-ss-integration.cloudfunctions.net/smartSuiteProxy', {
@@ -8,19 +21,11 @@ export async function callGoogleFunction(endpoint, method, data) {
             body: JSON.stringify({ endpoint, method, data })
         });
         if (!response.ok) {
-            throw new Error('HTTP error! Status: ' + response.status);
+            throw new Error(\`HTTP error! Status: \${response.status}\`);
         }
         return await response.json();
     } catch (error) {
-        console.error('Error during API call: ' + error.message);
+        console.error('Error during API call:', error);
         throw error;
     }
-}
-
-export async function fetchFromGoogleCloud(conversationId) {
-    const endpoint = '/api/v1/applications/64bea2c89335ca76865eedef/records/list/';
-    const method = 'POST';
-    const data = { limit: 2 };
-    const result = await callGoogleFunction(endpoint, method, { ...data, filter: { missiveId: conversationId } });
-    return result;
 }
