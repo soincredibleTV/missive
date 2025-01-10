466dae6dbc17fed259b503a428c7c0762fc2964d
soincredibleTV <developer@soincredible.tv>
2025-01-10 23:03:09 +0200

    Update events.js
    missive

--- events.js
+++ events.js
@@ -1,7 +1,23 @@
 import { fetchFromGoogleCloud, callGoogleFunction } from './core.js';
-import { updateCard, updateStatusClass, updateStageClass } from './ui.js';
+import { updateCard, updateStatusClass, updateStageClass, resetCard, toggleVisibility } from './ui.js';
 
 document.addEventListener('DOMContentLoaded', function() {
+    Missive.on('change:conversations', (ids) => {
+        Missive.fetchConversations(ids).then((conversations) => {
+            if (conversations.length !== 1) {
+                resetCard();
+                toggleVisibility(false);
+                return;
+            }
+            const conversationId = conversations[0].id;
+            if (currentConversationId === conversationId) return;
+            currentConversationId = conversationId;
+            resetCard();
+            toggleVisibility(true);
+            fetchFromGoogleCloud(conversationId);
+        });
+    });
+
     document.getElementById('card-status').addEventListener('change', (event) => {
         const newValue = event.target.value;
         updateSmartSuiteField(currentRecordId, 'sb2ebbc694', newValue);
